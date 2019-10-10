4f7cc5038ed9f6d5952d20d26bf5c1dfc49b8cab
Muhammad Rajab <ichijo_rajab@hotmail.com>
2019-10-10 20:51:44 +0700

    Initial Commit

---
+++ BelajarSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BelajarSwiftUI
+//
+//  Created by Muhammad Rajab Priharsanto on 10/10/19.
+//  Copyright © 2019 Muhammad Rajab Priharsanto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
